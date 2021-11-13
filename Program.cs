5b0a7b736988e52fe0026c3f66b64c2688373ea0
syedfaizz <syedfaizz45@gmail.com>
2021-11-13 15:42:26 +0530

    [faisal] Add: welcome

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+namespace LogicalPrograms
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the Number : ");
+            int number = int.Parse(Console.ReadLine());
+
+            int sum = number.ToString().Select(digit => int.Parse(digit.ToString())).ToArray().Sum();
+
+            Console.WriteLine($"The Sum of Digits is : {sum}");
+            Console.ReadKey();
+        }
+    }
+}
